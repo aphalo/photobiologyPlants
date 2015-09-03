14c42b24c6bab457981f473941040a9994b67fc5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2015-09-04 00:17:31 +0300

    Set type of absorbance objects to internal.

--- raw.data/read_cry_files.r
+++ raw.data/read_cry_files.r
@@ -2,13 +2,13 @@ library("photobiology")
 CRY2_dark.spct <- read.csv(file = "./raw.data/Cryptochromes/cry2_dark.csv",
                            header = TRUE, comment.char = "#")
 CRY2_dark.spct <- CRY2_dark.spct[-(1:2), -3]
-setFilterSpct(CRY2_dark.spct)
+setFilterSpct(CRY2_dark.spct, Tfr.type = "internal")
 CRY2_dark.spct <- interpolate_spct(CRY2_dark.spct, length.out = 100)
 
 CRY2_light.spct <- read.csv(file = "./raw.data/Cryptochromes/cry2_light.csv",
                             header = TRUE, comment.char = "#")
 CRY2_light.spct <- CRY2_light.spct[order(CRY2_light.spct$w.length), -3]
-setFilterSpct(CRY2_light.spct)
+setFilterSpct(CRY2_light.spct, Tfr.type = "internal")
 CRY2_light.spct <- interpolate_spct(CRY2_light.spct, length.out = 100)
 
 save(CRY2_dark.spct, CRY2_light.spct, file = "./data/cry.spct.rda")
@@ -19,7 +19,7 @@ save(CRY2_dark.spct, CRY2_light.spct, file = "./data/cry.spct.rda")
 load("./raw.data/UVR8/UVR8.raw.data.rda")
 UVR8_Glasgow.spct <- UVR8.raw.data[-64, ]
 names(UVR8_Glasgow.spct) <- c("w.length", "A")
-setFilterSpct(UVR8_Glasgow.spct)
+setFilterSpct(UVR8_Glasgow.spct, Tfr.type = "internal")
 UVR8_Glasgow.spct <- interpolate_spct(UVR8_Glasgow.spct, length.out = 100)
 
 save(UVR8_Glasgow.spct, file = "./data/UVR8.spct.rda")
